1596aeeddf211ae78e1171b886db95716d5dcc6e
Jason An <anzhe0629@gmail.com>
2016-08-27 13:01:50 -0700

    change Best Time

--- Best Time to Buy and Sell Stock/main.cpp
+++ Best Time to Buy and Sell Stock/main.cpp
@@ -7,28 +7,42 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
 
-        int price_min = prices[0];
-        int price_max = prices[0];
+        int sz = prices.size();
 
-        for (auto i = prices.begin();  i < prices.end() ; i++) {
-            if (price_max < *i)
-                price_max = *i;
-            if (price_min > *i)
-                price_min = *i;
-            price_max = price_max > *i ? price_max : *i;
-            price_min = price_min < *i ? price_min : *i;
+        if (sz == 0 || sz == 1)
+            return 0;
+
+        int price_min = prices[0];
+
+        int *profit = new int[sz]();
+        profit[0] = 0;
+        int best_return = 0;
+
+        for (auto i = 0;  i < sz; i++) {
+            if (price_min > *(prices.begin() + i))
+                price_min = *(prices.begin() + i);
+
+            if (i > 0) {
+                if (*(prices.begin() + i) > profit[i - 1])
+                    profit[i] = *(prices.begin() + i) - price_min;
+                else
+                    profit[i] = profit[i - 1];
+            }
+
+            best_return = best_return > profit[i] ? best_return : profit[i];
+
+
         }
 
-        if (price_max > price_min)
-            return price_max - price_min;
-        else
-            return 0;
+        delete []profit;
+        return best_return;
+
     }
 };
 
 int main(){
     Solution a;
-    vector<int> b = {1, 2, 3};
+    vector<int> b = {7, 1, 2, 6, 3};
 
     cout << a.maxProfit(b);
 }
